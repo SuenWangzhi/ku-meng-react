24cfdac7befc393778298ad9d025a8c9555035a7
Suen <1467820831@qq.com>
2021-11-01 23:39:08 +0800

    删除无用文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode> //纠正代码
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
